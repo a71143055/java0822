8c8926e981d65f7c912a8a44a1b7b1f7279d0e32
a71143055 <a71143055@gmail.com>
2024-10-10 09:16:39 +0900

    uf

---
+++ src/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    String name;
+    String breeds;
+    double weight;
+
+    void claw() {
+        System.out.println("할퀴기!!");
+    }
+
+    void meow() {
+        System.out.println("야옹~");
+    }
+}
